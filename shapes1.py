9ebcdbb8ec84d1da4f62147a458e35406d80e33f
Luistorresf <betotorresfi@gmail.com>
2020-10-27 18:09:09 -0700

    mensaje 1

--- shapes1.py
+++ shapes1.py
@@ -1,14 +1,24 @@
 import turtle
 from turtle import *
 from turtle import Screen
-from freegames import vector
     
 def emptySquare():
     pass  # TODO
     
 def filledSquare():
   pass  # TODO
-  
+
+def emptyCircle():
+    t = turtle.Turtle()
+    t.circle(100)
+
+def filledCircle():
+    t = turtle.Turtle()
+    t.fillcolor("red")
+    t.begin_fill()
+    t.circle(100)
+    t.end_fill()
+
 while True:
     screen = Screen()
     answer = screen.textinput("Next Game","1 - Square:")
@@ -18,4 +28,8 @@ while True:
         emptySquare()
     elif (answer == '2'):
         filledSquare()
+    elif (answer == "3"):
+        emptyCircle()
+    elif (answer == "4"):
+        filledCircle()
  
